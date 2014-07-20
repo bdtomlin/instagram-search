7b0eb3edce8862f5a299af7331536a95f4ef8329
bdtomlin <bdtomlin@gmail.com>
2014-07-19 19:18:24 -0500

    inital setup

---
+++ app.js
@@ -0,0 +1,6 @@
+// CLIENT ID 21560bbde39b44e1aeb207234486fa36
+// CLIENT SECRET	4c8adba06e3d4680b396804b4fd47b1b
+
+angular.module('instaSearch', ['ngAnimate'])
+  .controller('searchCtrl', function($scope){
+  });

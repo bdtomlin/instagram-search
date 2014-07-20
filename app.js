da3872819a3cd8dbb65e7624cf390ca6d2c58573
bdtomlin <bdtomlin@gmail.com>
2014-07-20 16:02:43 -0500

    completed initial project

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
-// CLIENT ID 21560bbde39b44e1aeb207234486fa36
-// CLIENT SECRET	4c8adba06e3d4680b396804b4fd47b1b
-
 angular.module('instaSearch', ['ngAnimate'])
-  .controller('searchCtrl', function($scope){
+  .controller('SearchCtrl', function($scope, $http){
+    var loadImages = function(){
+      var url = 'https://api.instagram.com/v1/tags/' + $scope.searchTerm + '/media/recent'
+      var params = {
+        client_id: '21560bbde39b44e1aeb207234486fa36',
+        callback: 'JSON_CALLBACK'
+      };
+
+      $http({
+        method: 'JSONP',
+        url: url,
+        params: params
+      }).
+      success(function(response){
+        $scope.lastSearch = $scope.searchTerm;
+        $scope.images = response.data;
+        $scope.loading = false;
+      }).
+      error(function(){
+        $scope.loading = false;
+        $scope.instaError = true;
+      });
+    };
+
+    $scope.searchInstagram = function(){
+      $scope.lastSearch = null;
+      $scope.images = [];
+      $scope.submitted = true;
+      $scope.instaError = false;
+      if($scope.searchForm.$valid){
+        $scope.submitted = false;
+        $scope.loading = true;
+        loadImages();
+      }
+    }
   });
